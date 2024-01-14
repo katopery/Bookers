7d6b0ebb94b97b5f63df830fe6cce6256cb8b8b9
MasatoKato <152849350+katopery@users.noreply.github.co>
2024-01-14 11:37:32 +0000

    [add] エラーメッセージ,バリデーション完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,20 +4,21 @@ class BooksController < ApplicationController
   end
 
   def create
-    @book_create = Book.new(book_params)
+    @book_new = Book.new(book_params)
 
-    if @book_create.save
+    if @book_new.save
       flash[:notice] = "Book was successfully created."
-      redirect_to book_path(@book_create.id)
+      redirect_to book_path(@book_new.id)
     else
+      @books = Book.all
       render :index
     end
 
   end
 
   def index
-    @book_index = Book.new
-    @books_index = Book.all
+    @book_new = Book.new
+    @books = Book.all
   end
 
   def show
